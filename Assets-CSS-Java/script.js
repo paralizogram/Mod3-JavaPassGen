4f83cba547f710578d4597236e5afd1478d299b0
Elizabeth Anacleto <122130720+paralizogram@users.noreply.github.com>
2023-04-17 15:17:13 -0400

    updated and cleared unnecessary notes and started working on Read Me

--- Assets-CSS-Java/script.js
+++ Assets-CSS-Java/script.js
@@ -1,29 +1,20 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate"); // link to html button id, line 28?
 
-// var charTypes = []; // array of arrays chosen through prompts
-// var length = []; // should I leave this? It functions without being listed here.
 var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var specialChar = [" ", "!", "", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^","`", "{", "|", "}", "~"]; // not sure how to deal with some characters on list, like spaces, double quotes, etc - double quoting each did not work,left a few out that were messing up the array, like " & _ symbols
 var numbers = [0,1,2,3,4,5,6,7,8,9]
 
-
   // Add event listener to generate button
   generateBtn.addEventListener("click", writePassword); // moved up, so flow is sequential. click button, generate password, then write password
   
-  
-  // Write password to the #password input
-  // do I need to add a randomizing feature for this function?
-
-  // MISSING something here. wrote writePassword() to console, and shows undefined
 function writePassword() {
   var password = generatePassword(); 
-  var passwordText = document.querySelector("#password"); // link to html id, line 22?
-
+  var passwordText = document.querySelector("#password"); 
+}
   passwordText.value = password; // display test "hello" instead of password worked
 
-}
   function generatePassword() {
     let charTypes = []; // array of arrays chosen through prompts
     let length = parseInt(prompt("Enter a number between 8 and 128 to set your password length:")); // converts string "100" to number 100
@@ -59,11 +50,3 @@ function writePassword() {
     return password; // display test "works" instead of password or true worked
   }
 
-
-// need to get input from user to generate password, https://www.w3schools.com/jsref/met_win_prompt.asp
-//function userPrompts() { Separate function for this or not? Tutor didn't suggest this
-
-
-// 2. When all above prompts answered, password generated and either:
-    // 1. Display in an alert (pop up?)?
-    // 2. Written to page? (in card where "Your secure password" initially written)
--- README.md
+++ README.md
@@ -1,2 +1,52 @@
 # Mod3-JavaPassGen
-Created repository
+# Javascript: Password Generator
+
+## Description
+
+This project was the assignment for Module 3.
+
+This was a **feature request Challenge** to refactor using an existing javascript starter code to make the password generator functional.
+
+The goal was to consider the given User Story, and meet the stated Acceptance Criteria.
+
+## User Story
+
+```
+AS A marketing agency
+I WANT a codebase that follows accessibility standards
+SO THAT our own site is optimized for search engines
+```
+
+## Acceptance Criteria
+
+```
+GIVEN a webpage meets accessibility standards
+WHEN I view the source code
+THEN I find semantic HTML elements
+WHEN I view the structure of the HTML elements
+THEN I find that the elements follow a logical structure independent of styling and positioning
+WHEN I view the icon and image elements
+THEN I find accessible alt attributes
+WHEN I view the heading attributes
+THEN they fall in sequential order
+WHEN I view the title element
+THEN I find a concise, descriptive title
+```
+ 
+The existing code was to be refactored to achieve the above criteria while maintaining functionality, and maintaining the design as laid out in the following mock-up. Link to the application: 
+https://donefu.github.io/Refactor-Website/
+
+![webpage](./assets/01-html-css-git-homework-demo.png)
+
+## Installation
+
+N/A
+
+## Credits
+
+N/A
+
+## License
+MIT 
+
+Please refer to the LICENSE in the repo. 
